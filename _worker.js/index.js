21f6ff853f6da735c42f9faabdcd2a92b79a28ef
Cole Roberts <cole.alan.roberts@gmail.com>
2025-04-30 17:03:51 -0500

    update site

---
+++ _worker.js/index.js
@@ -0,0 +1,33 @@
+globalThis.process ??= {}; globalThis.process.env ??= {};
+import { renderers } from './renderers.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BuDNbokm.mjs';
+import { manifest } from './manifest_DNVh9Ky_.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/about.astro.mjs');
+const _page1 = () => import('./pages/posts/japan.astro.mjs');
+const _page2 = () => import('./pages/posts/niseko.astro.mjs');
+const _page3 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/about/index.astro", _page0],
+    ["src/pages/posts/japan.md", _page1],
+    ["src/pages/posts/niseko.md", _page2],
+    ["src/pages/index.astro", _page3]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_astro-internal_middleware.mjs')
+});
+const _args = undefined;
+const _exports = createExports(_manifest);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
