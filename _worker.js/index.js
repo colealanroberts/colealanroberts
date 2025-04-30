c781f07966780b56a0ae1567dfa427b1ecd1967b
Cole Roberts <cole.alan.roberts@gmail.com>
2025-04-30 18:53:28 -0500

    updates

--- _worker.js/index.js
+++ _worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BuDNbokm.mjs';
-import { manifest } from './manifest_DPSVMKur.mjs';
+import { manifest } from './manifest_vmn1eyYe.mjs';
 
 const serverIslandMap = new Map();;
 
